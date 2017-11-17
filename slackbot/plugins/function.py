47293c3e656ccbbf1f6d12660daba65829d6a38b
sun-yryr <texiyo0425@yahoo.co.jp>
2017-11-18 01:49:56 +0900

    touban

--- slackbot/plugins/function.py
+++ slackbot/plugins/function.py
@@ -1,4 +1,10 @@
 # coding: utf-8
-import sys
-sys.path.append('../..')
-import 
+import sys,os
+sys.path.append(os.pardir)
+import touban as t
+
+def touban():
+    msg = t.m_print()
+    #print -> slack.message
+    print msg[0]
+    print msg[1]
---
+++ slackbot/touban.py
@@ -0,0 +1,70 @@
+# coding:utf-8
+import os
+
+#定期投稿用
+def main():
+    members = openmember()
+    a = ""
+    b = ""
+    count = opennumber()
+    for i in range(5):
+        a += "[%d]%s " % (count[0],members[count[0]-1])
+        count[0] += 1
+        if count[0]==(len(members)+1):
+            count[0] = 1
+    for i in range(2):
+        b += "[%d]%s " % (count[1],members[count[1]-1])
+        count[1] += 1
+        if count[1]==(len(members)+1):
+            count[1] = 1
+    #print -> slack.message
+    print a
+    print b
+
+    os.remove("/Users/sun-mba/develop/bot/slackbot/plugins/num.txt")
+    f = open("/Users/sun-mba/develop/bot/slackbot/plugins/num.txt","w")
+    f.write(str(count[0]))
+    f.write('\n')
+    f.write(str(count[1]))
+    f.close()
+
+def openmember():
+    f = open("/Users/sun-mba/develop/bot/slackbot/plugins/3j.txt","r")
+    members = f.readlines()
+    f.close()
+    for i in range(len(members)):
+        members[i] = members[i].replace('\n','')
+        members[i] = members[i].replace('\r','')
+    return members
+
+def opennumber():
+    f = open("/Users/sun-mba/develop/bot/slackbot/plugins/num.txt","r")
+    num = f.readlines()
+    f.close()
+    for i in range(len(num)):
+        num[i] = num[i].replace('\n','')
+        num[i] = num[i].replace('\r','')
+        num[i] = int(num[i])
+    return num
+
+#slackbot用
+def m_print():
+    members = openmember()
+    a = ""
+    b = ""
+    count = opennumber()
+    for i in range(5):
+        a += "[%d]%s " % (count[0],members[count[0]-1])
+        count[0] += 1
+        if count[0]==(len(members)+1):
+            count[0] = 1
+    for i in range(2):
+        b += "[%d]%s " % (count[1],members[count[1]-1])
+        count[1] += 1
+        if count[1]==(len(members)+1):
+            count[1] = 1
+    msg = [a,b]
+    return msg
+
+if __name__ == '__main__':
+    main()
